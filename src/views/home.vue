3ebdf85d1a7178ff6b567a36512f98f28944798e
awaylee <969603910@qq.com>
2018-01-03 14:26:13 +0800

    添加iconfont图标

---
+++ src/views/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+    <header>
+      <div class="left"></div>
+      <div class="right"></div>
+      <div></div>
+    </header>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
+
+<style>
+  header{
+    width: 100%;
+    height: 2.875rem;
+    background: #d62f10;
+  }
+</style>
