2a9309357a64ca16e7c7604870250694b4747cf5
jenkins <526269310@qq.com>
2018-01-03 14:15:53 +0800

    图标样式添加

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="header">
+		<i class="icon iconfont icon-zuo"></i>
+	</div>
+</template>
+<script>
+	import '@/assets/style/header.css'
+	export default {
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="home">
+		<div class="aaa">
+			123
+		</div>
+		<Header></Header>
+	</div>
+</template>
+<script>
+	import header from '@/components/header.vue'
+	export default {
+		name: 'home',
+		data() {
+			return {
+				
+			}
+		},
+		components: {
+			'Header': header
+		}
+	}
+</script>
+<style>
+	.aaa{
+		position: fixed;
+		top: 50px;
+	}
+</style>
